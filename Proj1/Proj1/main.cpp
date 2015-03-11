f3adff725f33f28db51f833815717d50344a077c
Jack Moore <jtmthf@mst.edu>
2015-03-10 23:18:21 -0500

    Initial commit

---
+++ Proj1/Proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj1
+//
+//  Created by Jack Moore on 3/10/15.
+//  Copyright (c) 2015 Jack Moore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
